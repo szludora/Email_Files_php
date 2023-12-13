c19922a48b759b793e6af400f900837e9239b0cd
szludora <szlucska.dora@gmail.com>
2023-12-13 19:11:47 +0100

    foreach

--- backend/app/Http/Controllers/MailController.php
+++ backend/app/Http/Controllers/MailController.php
@@ -16,9 +16,15 @@ class MailController extends Controller
             'body' => 'This is for testing email using smtp.'
         ];
 
-        Mail::to('****@gmail.com')
-            ->send(new DemoMail($mailData));
+        foreach ([
+            "patrikhamar460@gmail.com", "beasuhi333@gmail.com", "kissadam990202@gmail.com", "my.temporary.test.mailbox+vajon.kell.a.pluszjel@gmail.com",
+            "testemil9779@gmail.com", "tesztmarci96@gmail.com", "tesztbibanka@gmail.com", "foloslegesideirni@gmail.com", "tesztmariann7474@gmail.com",
+            "eakos2131@gmail.com", "tesztgyongyiteszt@gmail.com", "t3szt3l3m@gmail.com", "testszzoli2023@gmail.com",
+        ] as $recipient) {
 
+            Mail::to($recipient)
+                ->send(new DemoMail($mailData));
+        }
         dd("Email is sent successfully.");
     }
 }
